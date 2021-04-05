dd8c8e6f8b4073d8d4bedfb4b8da157e24157a34
Shad0n <tom31245@gmail.com>
2021-04-05 16:04:43 +0300

    pr6 attempt

---
+++ src/pr6/factoryMethod/ItalianMotorcycle.java
@@ -0,0 +1,7 @@
+package pr6.factoryMethod;
+
+public class ItalianMotorcycle implements Motorcycle {
+    public void produce() {
+        System.out.println("That's an italian bike!");
+    }
+}
---
+++ src/pr6/factoryMethod/JapaneseMotorcycle.java
@@ -0,0 +1,7 @@
+package pr6.factoryMethod;
+
+public class JapaneseMotorcycle implements Motorcycle{
+    public void produce() {
+        System.out.println("That's a japanese bike!");
+    }
+}
